3b5b6a5743e5ee3ab34b3b1140fde66943b92485
yuenloon <kiewyl@gmail.com>
2025-01-07 10:50:47 +0800

    Update investmentscript.js

--- investmentscript.js
+++ investmentscript.js
@@ -3,6 +3,8 @@ const supabaseUrl = 'https://fhawjjmjijeqfqoycbkw.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZoYXdqam1qaWplcWZxb3ljYmt3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzYxNDI2NjksImV4cCI6MjA1MTcxODY2OX0.jPqFRNkRgNM7jYHByfq4ihNfdr6vKlYDjr-77T-p-20';  
 const supabase = Supabase.createClient(supabaseUrl, supabaseKey);  
 
+console.log('Supabase client initialized:', supabase);  
+
 document.getElementById('investmentForm').addEventListener('submit', async function(event) {  
     event.preventDefault(); // Prevent the form from submitting normally  
 
